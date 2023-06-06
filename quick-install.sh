796e06fba664357d56ddde10e6f46958ba004388
Li Dong <cnlidong@live.cn>
2023-06-06 21:33:53 +0800

    init

---
+++ quick-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+## 克隆仓库到本地
+echo "clone repository..."
+git clone --depth=1 https://github.com/xyhelper/chatgpt-api-server-deploy.git chatgpt-api
+
+## 进入目录
+cd chatgpt-mirror
+
+docker compose pull
+docker compose up -d
+
+## 提示信息
+echo "服务启动成功，请访问 http://localhost:8100"
+echo "管理员后台地址 http://localhost:8100/xyhelper"
+echo "管理员账号: admin"
+echo "管理员密码: 123456"
+echo "请及时修改管理员密码"
